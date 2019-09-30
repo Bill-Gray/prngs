3b7a0aa2260ac9fcd3edecf08dbe7f42efc8e3d3
Bill-Gray <pluto@projectpluto.com>
2019-09-30 17:17:33 -0400

    'random.c' (code to dump random data from /dev/random or /dev/urandom) : Improved command-line handling and the error message.

--- random.c
+++ random.c
@@ -3,6 +3,11 @@
 #include <string.h>
 #include <assert.h>
 
+/* Code to read random bytes from /dev/random or /dev/urandom,  and
+output them in a desired radix.  (By default,  that's hexadecimal,
+but there's a command line option for decimal,  octal,  binary,
+etc.)   */
+
 static void show_poolsize( void)
 {
    FILE *ifile = fopen( "/proc/sys/kernel/random/entropy_avail", "rb");
@@ -26,11 +31,24 @@ static const char *digits =
    "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
    "+/!\"#$%&'()*,-.:;<=>?@[\\]^_`{|}";
 
+static void err_exit( void)
+{
+   printf( "Options are :\n");
+   printf( "   -l(lines)  Number of lines to output (default=10)\n");
+   printf( "   -c(count)  Line length (default=64)\n");
+   printf( "   -r(radix)  Radix (default=16,  i.e.,  hex)\n");
+   printf( "   -u         Use /dev/urandom,  not /dev/random\n");
+   printf( "Use -r10 to get random digits.  Max radix is %d.\n",
+                          (int)strlen( digits));
+   exit( -1);
+}
+
 int main( const int argc, const char **argv)
 {
 
    FILE *ifile;
    const char *ifilename = "/dev/random";
+   const unsigned max_radix = strlen( digits);
    unsigned i, j;
    unsigned count = 64, n_lines = 10;
    unsigned radix = 16;   /* default to hex output */
@@ -45,14 +63,23 @@ int main( const int argc, const char **argv)
             case 'l':
                n_lines = atoi( argv[i] + 2);
                break;
+            case 'c':
+               count = atoi( argv[i] + 2);
+               break;
+            case 'r':
+               radix = atoi( argv[i] + 2);
+               if( radix > max_radix || radix < 2)
+                  {
+                  printf( "Radix must be in the range 2 to %u\n", max_radix);
+                  err_exit( );
+                  }
+               break;
             case 'u':
                ifilename = "/dev/urandom";
                break;
             default:
-               if( atoi( argv[i] + 1))
-                  radix = (unsigned)atoi( argv[i] + 1);
-               else
-                  printf( "Unrecognized option '%s'\n", argv[i]);
+               printf( "Unrecognized option '%s'\n", argv[i]);
+               err_exit( );
                break;
             }
    show_poolsize( );
